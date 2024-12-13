567516c807e79d5feb7415d72da211a4c1d26a09
Karuppanan007 <karuppusurya007@gmail.com>
2024-12-13 14:15:25 +0530

    update

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import "./Loader.scss";
-// import { loader } from "../../utils/images";
+
 
 const Loader = () => {
   return (
     <div className='loader my-5'>
       <div className='container flex align-center justify-center'>
         {/* <img src = {loader} alt = "" /> */}
-        <div class="blob">
+        <div className="blob">
 
         </div>
       </div>
@@ -16,4 +16,4 @@ const Loader = () => {
   )
 }
 
-export default Loader
+export default Loader;
